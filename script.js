9214bdd055cc4e30e929a43e39127a2b2f6a20bb
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-03-04 15:03:20 +0100

    asd

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const sectionList = document.getElementById('sectionList')
 const sectionNew = document.getElementById('sectionNew')
 const sectionEdit = document.getElementById('sectionEdit')
 const productTableBody = document.getElementById('productTableBody')
+const submitNewButton = document.getElementById('submitNewButton')
+const newName =  document.getElementById('newName')
 
 const listLink = document.getElementById('listLink')
 const newLink = document.getElementById('newLink')
@@ -28,15 +30,32 @@ newLink.addEventListener("click",()=>{
     showSection('sectionList');    
 });
 
+
+submitNewButton.addEventListener("click",()=>{ 
+    items.push(newName.value);
+    renderTr(newName.value);
+    showSection('sectionList');    
+});
+
+
+function renderTr(item){
+    let template = `<tr><td>${item}</td></tr>`
+    productTableBody.innerHTML = productTableBody.innerHTML + template;
+} 
 // 
-items = ["Stefan", "Kalle", "Lisa" ] 
+items = ["Stefan", "Kalle", "Lisa" ] ;
+items.forEach( (item) => {
+    renderTr(item);
+}  );
+
+
 //Loopa den
 // för varje skapa tr, för varje skapa td:s 
 //lägga in den nya tr:n som ett barn till  productTableBody
 
   
 
-showSection('sectionList')
+showSection('sectionList');
 
 // 2. funktion showSection
 // 3. events = händelsestyrd programmering
